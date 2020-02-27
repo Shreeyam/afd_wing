97363db37d9f64751f9a416677d8571669fb41c9
Yuri@Surface <yuri.shimane@gmail.com>
2020-02-27 23:18:03 +0000

    cfrp material files

---
+++ cfrpudud90.m
@@ -0,0 +1,14 @@
+% Material file - CFRPUDUD90
+materialname = 'cfrpudud90';
+% Young's Modulus [GPa]
+E = 8.5;
+% Shear modulus [MPa]
+G = 5350;
+% yield strength [N/mm^2]
+sigma_yield = 32;
+% UTS [N/mm^2]
+UTS = 32;
+% max shear strength [N/mm^2]
+taumax = 0.65*UTS;
+% density [kg/m^3]
+density_kgm3 = 1590;
---
+++ cfrpwovenqi.m
@@ -0,0 +1,14 @@
+% Material file - CFRPwovenQI
+materialname = 'cfrpwovenqi';
+% Young's Modulus [GPa]
+E = 51.4;
+% Shear modulus [MPa]
+G = 18100;
+% yield strength [N/mm^2]
+sigma_yield = 371;
+% UTS [N/mm^2]
+UTS = 371;
+% max shear strength [N/mm^2]
+taumax = 0.65*UTS;
+% density [kg/m^3]
+density_kgm3 = 1540;
